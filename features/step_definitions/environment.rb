40dab106065f8ee91c3f16c11b79f52d81ac1b06
Alistair Israel <aisrael@gmail.com>
2024-05-07 18:38:53 -0400

    Try again

--- features/step_definitions/environment.rb
+++ features/step_definitions/environment.rb
@@ -4,5 +4,11 @@
 # This is necessary to run the tests in GitHub Actions which does _not_ allow
 # environments variables starting with `GITHUB_`.
 Given /a valid GITHUB_TOKEN is set/ do
-    ENV['GITHUB_TOKEN'] = ENV['CUCUMBER_GITHUB_TOKEN'] unless ENV['GITHUB_TOKEN']
+    unless ENV['GITHUB_TOKEN']
+        if ENV['CUCUMBER_GITHUB_TOKEN']
+            ENV['GITHUB_TOKEN'] = ENV['CUCUMBER_GITHUB_TOKEN']
+        else
+            raise "No GITHUB_TOKEN or CUCUMBER_GITHUB_TOKEN environment variable set!"
+        end
+    end
 end
